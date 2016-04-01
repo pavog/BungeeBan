0e96b5371c6e92c7519d82366aa9579a504c0115
Luca Vinciguerra <lucavinci1@gmail.com>
2016-04-01 22:44:12 +0200

    initial commit

---
+++ src/de/vincidev/bungeeban/BungeeBan.java
@@ -0,0 +1,16 @@
+package de.vincidev.bungeeban;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeBan extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
